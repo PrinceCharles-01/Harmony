54b0d7946abd13d214683ff5ee25c23c1fabcd00
Charles Lewis Sono <charles.lewis.sono@gmail.com>
2025-10-08 15:11:57 +0100

    fix(db): Rétablissement de la configuration de l'hôte de la base de données pour Docker

--- Harmony_backend/harmony_backend/settings.py
+++ Harmony_backend/harmony_backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME', 'harmony_db'),
         'USER': os.environ.get('DATABASE_USER', 'harmony_user'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'harmony_password'),
-        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
 }
